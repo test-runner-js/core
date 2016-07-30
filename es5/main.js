011c8fd01a10076866ce1f23e88869885f9f619e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-07-30 16:39:41 +0100

    typo

--- es5/main.js
+++ es5/main.js
@@ -3,8 +3,8 @@
 var detect = require('feature-detect-es6');
 
 if (detect.all('class', 'arrowFunction', 'let', 'const', 'collections')) {
-  module.exports = require('./lib/test-runnner');
+  module.exports = require('./lib/test-runner');
 } else {
   require('core-js/es6/object');
-  module.exports = require('../es5/lib/test-runnner');
+  module.exports = require('../es5/lib/test-runner');
 }
