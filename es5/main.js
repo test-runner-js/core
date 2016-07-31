0d8296ebcac7d9ccc56447d9c08baee34d58211e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-07-31 13:31:22 +0100

    feature detect [].includes()

--- es5/main.js
+++ es5/main.js
@@ -2,6 +2,10 @@
 
 var detect = require('feature-detect-es6');
 
+if (!Array.prototype.includes) {
+  require('core-js/es7/array');
+}
+
 if (detect.all('class', 'arrowFunction', 'let', 'const', 'collections', 'destructuring')) {
   module.exports = require('./lib/test-runner');
 } else {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 'use strict'
 var detect = require('feature-detect-es6')
 
+if (!Array.prototype.includes) {
+  require('core-js/es7/array')
+}
+
 if (detect.all('class', 'arrowFunction', 'let', 'const', 'collections', 'destructuring')) {
   module.exports = require('./lib/test-runner')
 } else {
