0c0a91bda9eab034c5d574a6b6e2e29fc8f757fe
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-08-07 20:40:49 +0100

    added polyfills

--- es5/main.js
+++ es5/main.js
@@ -6,6 +6,13 @@ if (!Array.prototype.includes) {
   require('core-js/es7/array');
 }
 
+if (!detect.collections()) {
+  require('core-js/es6/map');
+}
+if (!detect.promises()) {
+  require('core-js/es6/promise');
+}
+
 if (detect.all('class', 'arrowFunction', 'let', 'const', 'collections', 'destructuring')) {
   module.exports = require('./lib/test-runner');
 } else {
