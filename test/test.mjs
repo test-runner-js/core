9a31ecccab609adb270395f305ccadd710c7559e
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-07-21 19:58:21 +0100

    failing test

--- test/test.mjs
+++ test/test.mjs
@@ -2,19 +2,45 @@ import Test from '../lib/test.mjs'
 import a from 'assert'
 
 {
-  const test = new Test('simple', function () {
+  const test = new Test('passing sync test', function () {
     return true
   })
-  const result = test.run()
-  a.strictEqual(result, true)
+  test.run().then(result => {
+    a.strictEqual(result, true)
+  })
 }
 
 {
-  const test = new Test('failing test', function () {
+  const test = new Test('failing sync test', function () {
     throw new Error('failed')
   })
-  a.throws(
-    () => test.run(),
-    /failed/
-  )
+  test.run()
+    .then(() => {
+      a.fail("shouldn't reach here")
+    })
+    .catch(err => {
+      a.ok(/failed/.test(err.message))
+    })
+}
+
+{
+  const test = new Test('passing async test', function () {
+    return Promise.resolve(true)
+  })
+  test.run().then(result => {
+    a.strictEqual(result, true)
+  })
+}
+
+{
+  const test = new Test('failing async test', function () {
+    return Promise.reject(new Error('failed'))
+  })
+  test.run()
+    .then(() => {
+      a.fail("shouldn't reach here")
+    })
+    .catch(err => {
+      a.ok(/failed/.test(err.message))
+    })
 }
