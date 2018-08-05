c22e2228fb70a69e9e9f284a4cdffb90b568ec30
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-08-05 16:14:30 +0100

    runner tests

--- test/test-runner.mjs
+++ test/test-runner.mjs
@@ -2,11 +2,35 @@ import TestRunner from '../lib/test-runner.mjs'
 import a from 'assert'
 
 {
-  const runner = new TestRunner()
+  const runner = new TestRunner('runner.run: one test')
   runner.test('simple', function () {
     return true
   })
-  const results = await runner.run()
-  console.log(results)
-  a.strictEqual(results[0], true)
+  runner.run()
+    .then(results => {
+      a.strictEqual(results[0], true)
+    })
+    .catch(err => {
+      console.log(err)
+      a.fail('should not reach here')
+    })
+}
+
+{
+  const runner = new TestRunner('runner.run: two tests')
+  runner.test('simple', function () {
+    return true
+  })
+  runner.test('simple 2', function () {
+    return 1
+  })
+  runner.run()
+    .then(results => {
+      a.strictEqual(results[0], true)
+      a.strictEqual(results[1], 1)
+    })
+    .catch(err => {
+      console.log(err)
+      a.fail('should not reach here')
+    })
 }
